ceee7fead5d8b786383dc89fd85b7479d29234ec
Kevin Ostafinski <kevscripted@gmail.com>
2024-03-29 19:07:49 +0100

    button component without icon or polymorphism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main className="border-4 w-full border-red-500 h-screen p-8">
-      <button>Rejoindre notre Discord</button>
+      <div className="flex p-4 gap-4">
+        <Button>Rejoindre notre Discord</Button>
+        <Button variant="secondary">Rejoindre notre Discord</Button>
+      </div>
+      <div className="flex bg-navy-900 p-4 gap-4">
+        <Button theme="dark">Rejoindre notre Discord</Button>
+        <Button theme="dark" variant="secondary">
+          Rejoindre notre Discord
+        </Button>
+      </div>
     </main>
   );
 }
