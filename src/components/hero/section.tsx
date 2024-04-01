899e3995a2eb734b348c163613ba8f840a2e67e6
Kevin Ostafinski <kevscripted@gmail.com>
2024-04-01 15:45:38 +0200

    working mobile video-blob

--- src/components/hero/section.tsx
+++ src/components/hero/section.tsx
@@ -1,11 +1,12 @@
 import { Button } from "../button";
 import { DiscordIcon } from "../icons/discord";
+import { VideoBlob } from "./video-blob";
 
 export function HeroSection() {
   return (
     <section className="relative bg-navy-900 bg-gradient-to-br from-navy-700 to-navy-900 pt-20 pb-16 rounded-bl-2xl">
       <div className="wrapper flex flex-col items-center gap-12">
-        <div className="w-40 h-40 rounded-full bg-navy-700"></div>
+        <VideoBlob />
 
         <div className="w-full flex flex-col items-center text-center text-white">
           <h1 className="text-5xl font-bold tracking-tighter">AideEducation</h1>
---
+++ src/components/hero/video-blob.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { ExitIcon } from "../icons/exit";
+import { PlayIcon } from "../icons/play";
+
+export function VideoBlob() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [videoStatus, setVideoStatus] = useState<"open" | "close">("close");
+
+  function handleVideoOpen() {
+    // remove scrollbar when video open
+    document.body.classList.add("overflow-y-hidden");
+    document.documentElement.classList.add("overflow-y-hidden");
+    setVideoStatus("open");
+  }
+
+  function handleVideoClose() {
+    document.body.classList.remove("overflow-y-hidden");
+    document.documentElement.classList.remove("overflow-y-hidden");
+    setVideoStatus("close");
+  }
+
+  return (
+    <>
+      <div
+        className="group hover:scale-125 duration-300 transition w-48 aspect-square bg-navy-700 blobing flex justify-center items-center cursor-pointer"
+        onClick={handleVideoOpen}
+      >
+        <PlayIcon className="w-8 h-8 fill-white md:fill-navy-900 group-hover:fill-white transition" />
+      </div>
+
+      {videoStatus === "open" && (
+        <div className="fixed z-50 inset-0 bg-gradient-to-b from-navy-700 to-navy-900 flex justify-center items-center p-8">
+          <div
+            className="absolute top-8 right-8 flex justify-center items-center z-50 cursor-pointer"
+            onClick={handleVideoClose}
+          >
+            <ExitIcon className="w-8 h-8 fill-white" />
+          </div>
+
+          <div className="w-full aspect-video">
+            <video
+              ref={videoRef}
+              className="w-full h-auto bg-navy-900"
+              controls
+              // poster="https://source.unsplash.com/random/1280x720"
+            >
+              <source src="/presentation.mp4" />
+            </video>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
