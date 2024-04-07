1df9f36799d9c8af3a117582fd5ee78540b5c010
Kevin Ostafinski <kevscripted@gmail.com>
2024-04-07 23:43:30 +0200

    added members filter

--- src/app/nous/equipe/page.tsx
+++ src/app/nous/equipe/page.tsx
@@ -2,9 +2,78 @@ import { getMembers } from "@/cms/api";
 import { MemberCard } from "@/components/equipe/member-card";
 import { DepartmentFilter } from "@/components/equipe/department-filter";
 import { PageHeader } from "@/components/page-header";
+import { Member } from "@/cms/types";
+import { SortFilter } from "@/components/equipe/sort-filter";
 
-export default async function EquipePage() {
+const departments = [
+  { value: "direction-generale", label: "Direction Générale" },
+  { value: "departement-pedagogique", label: "Dept. Pédagogique" },
+  { value: "departement-interieur", label: "Dept. Intérieur" },
+  { value: "departement-exterieur", label: "Dept. Extérieur" },
+];
+
+const sorters: { value: keyof Member; label: string }[] = [
+  { value: "lastname", label: "Nom" },
+  { value: "joinedAt", label: "Date d'arrivée" },
+  { value: "rank", label: "Rang" },
+];
+
+type EquipePageSearchParams = {
+  department: string | undefined;
+  sort: keyof Member | undefined;
+};
+
+type EquipePageProps = {
+  searchParams: EquipePageSearchParams;
+};
+
+function setFilterOptions(searchParams: EquipePageSearchParams) {
+  const filterOptions = {} as EquipePageSearchParams;
+  if (searchParams.department) {
+    filterOptions.department =
+      departments.find((d) => d.value === searchParams.department)?.value ||
+      undefined;
+  }
+  if (searchParams.sort) {
+    filterOptions.sort =
+      sorters.find((s) => s.value === searchParams.sort)?.value || undefined;
+  } else {
+    filterOptions.sort = "lastname";
+  }
+  return filterOptions;
+}
+
+function setFilteredMembers(
+  members: Member[],
+  filterOptions: EquipePageSearchParams
+) {
+  const filteredMembers: Member[] = members.filter((m) => {
+    const hasDepartment = filterOptions.department
+      ? m.department === filterOptions.department
+      : true;
+    return hasDepartment;
+  });
+
+  if (filterOptions.sort) {
+    filteredMembers.sort((a, b) => {
+      if (filterOptions.sort === "joinedAt") {
+        return new Date(a.joinedAt) > new Date(b.joinedAt) ? -1 : 1;
+      }
+      if (filterOptions.sort === "rank") {
+        return Number(a.rank) > Number(b.rank) ? -1 : 1;
+      }
+      return a["lastname"] > b["lastname"] ? 1 : -1;
+    });
+  }
+
+  return filteredMembers;
+}
+
+export default async function EquipePage({ searchParams }: EquipePageProps) {
   const members = await getMembers();
+
+  const filterOptions = setFilterOptions(searchParams);
+  const filteredMembers = setFilteredMembers(members, filterOptions);
 
   return (
     <main className="pb-24">
@@ -29,14 +98,20 @@ export default async function EquipePage() {
         </div>
       </PageHeader>
 
-      <div className="relative mt-12 lg:mt-16">
-        <DepartmentFilter />
+      <div className="relative mt-12 lg:mt-16 flex flex-col gap-8">
+        <div className="wrapper flex justify-end">
+          <SortFilter sorters={sorters} activeSort={filterOptions.sort} />
+        </div>
+        <DepartmentFilter
+          departments={departments}
+          activeDepartment={filterOptions.department}
+        />
       </div>
 
       <div className="wrapper mt-8">
         <ul className="w-full grid gap-1 md:gap-2 grid-cols-1 md:grid-cols-2 lg:grid lg:grid-cols-3 lg:gap-4 auto-rows-fr">
-          {members.length &&
-            members.map((member) => (
+          {filteredMembers.length &&
+            filteredMembers.map((member) => (
               <MemberCard member={member} key={member.id} />
             ))}
         </ul>
--- src/components/equipe/department-filter.tsx
+++ src/components/equipe/department-filter.tsx
@@ -1,29 +1,61 @@
 "use client";
+import { usePathname, useRouter } from "next/navigation";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-export function DepartmentFilter() {
+type DepartmentFilterProps = {
+  departments: any[];
+  activeDepartment?: string;
+};
+
+export function DepartmentFilter({
+  departments,
+  activeDepartment,
+}: DepartmentFilterProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  function handleDepartment(newDepartment?: string) {
+    const currentParams = new URLSearchParams(window.location.search);
+    if (!newDepartment) {
+      currentParams.delete("department");
+    } else {
+      currentParams.set("department", newDepartment);
+    }
+
+    router.replace(`${pathname}?${currentParams.toString()}`, {
+      scroll: false,
+    });
+  }
+
   return (
     <ScrollContainer
       className="scroll-container select-none flex flex-row flex-nowrap list-none gap-8 scroll-smooth"
       activationDistance={2}
     >
-      <div className="w-full h-full flex wrapper gap-4">
-        <li className="px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50">
+      <ul className="w-full h-full flex wrapper gap-4">
+        <li
+          onClick={() => handleDepartment()}
+          className={`px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50 ${
+            !activeDepartment ? "text-navy-700" : "text-neutral-900"
+          }`}
+        >
           <span className="text-sm">Tous</span>
         </li>
-        <li className="px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50">
-          <span className="text-sm">Direction Générale</span>
-        </li>
-        <li className="px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50">
-          <span className="text-sm">Dept. Pédagogique</span>
-        </li>
-        <li className="px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50">
-          <span className="text-sm">Dept. Intérieur</span>
-        </li>
-        <li className="px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50">
-          <span className="text-sm">Dept. Exterieur</span>
-        </li>
-      </div>
+        {departments.length &&
+          departments.map((dept) => (
+            <li
+              key={dept.value}
+              onClick={() => handleDepartment(dept.value)}
+              className={`px-4 py-2 cursor-pointer rounded-full shrink-0 flex justify-center items-center bg-neutral-50  ${
+                activeDepartment === dept.value
+                  ? "text-navy-700"
+                  : "text-neutral-900"
+              }`}
+            >
+              <span className="text-sm">{dept.label}</span>
+            </li>
+          ))}
+      </ul>
     </ScrollContainer>
   );
 }
